9c4ecf2cf3450a7326c71c3f21f24d324001d7e5
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-12 16:45:02 +0700

    feat(search): filter monsters

--- src/App.js
+++ src/App.js
@@ -21,13 +21,18 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monsters => 
+      monsters.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    
     return (
       <div className='App'>
         <input
           type='search'
           placeholder='search monsters'
           onChange={ e => this.setState({ searchField: e.target.value  }) } />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
