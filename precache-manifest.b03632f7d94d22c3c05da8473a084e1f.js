7cc16bfcb38b09797140c2bdb153f00cfe385cd0
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-05-15 01:55:19 +0700

    Updates

---
+++ precache-manifest.b03632f7d94d22c3c05da8473a084e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41d904fa164f882b6f943a090bcc98a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4b21b99d48c829f2afe",
+    "url": "/monsters-rolodex/static/css/main.e9425aee.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b21b99d48c829f2afe",
+    "url": "/monsters-rolodex/static/js/main.d95d86b8.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
